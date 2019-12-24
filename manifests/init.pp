91197ba269d594b4d414f3559bf27e49f137c400
PaulWalkerUK <walker.paul+github@gmail.com>
2019-12-24 01:11:26 +0000

    Update docs

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,8 @@
 # @summary This class manages xfce
 #
-# @example
+# This is very simple and at the moment only sets up the base xfce4
+#
+# @example Basic usage
 #   include xfce
 class xfce {
   contain xfce::install
