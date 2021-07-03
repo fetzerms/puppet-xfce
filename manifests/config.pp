4859976ad7468ae3f13c858779f72563bf249851
Matthias Fetzer <fetzerms@users.noreply.github.com>
2021-07-03 08:01:17 +0200

    Update config.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,17 +2,5 @@
 #
 # @api private
 class xfce::config {
-  if ($xfce::display_manager != '') and (defined(Service[$xfce::display_manager])) {
-    exec { 'set_font':
-      command => 'xfconf-query --channel xsettings --property /Gtk/FontName --set "Cantarell 10"',
-      path    => [ '/usr/local/bin/', '/bin/', '/usr/bin/' ],
-      before  => Service[$xfce::display_manager],
-    }
-  }
-  else {
-    exec { 'set_font':
-      command => 'xfconf-query --channel xsettings --property /Gtk/FontName --set "Cantarell 10"',
-      path    => [ '/usr/local/bin/', '/bin/', '/usr/bin/' ],
-    }
-  }
+
 }
