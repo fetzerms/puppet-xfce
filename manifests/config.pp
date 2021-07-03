a962951feb365699b51395e2002401929601f353
Matthias Fetzer <fetzerms@users.noreply.github.com>
2021-07-03 07:51:37 +0200

    Add '/usr/bin/' to path

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,7 +5,7 @@ class xfce::config {
   if ($xfce::display_manager != '') and (defined(Service[$xfce::display_manager])) {
     exec { 'set_font':
       command => 'xfconf-query --channel xsettings --property /Gtk/FontName --set "Cantarell 10"',
-      path    => [ '/usr/local/bin/', '/bin/' ],
+      path    => [ '/usr/local/bin/', '/bin/', '/usr/bin/' ],
       user    => 'vagrant',
       before  => Service[$xfce::display_manager],
     }
@@ -13,7 +13,7 @@ class xfce::config {
   else {
     exec { 'set_font':
       command => 'xfconf-query --channel xsettings --property /Gtk/FontName --set "Cantarell 10"',
-      path    => [ '/usr/local/bin/', '/bin/' ],
+      path    => [ '/usr/local/bin/', '/bin/', '/usr/bin/' ],
       user    => 'vagrant',
     }
   }
